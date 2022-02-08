8b7236800c60f5e60d1e17ece66e3d0288cdef9b
ChosonIgor <51879005+ChosonIgor@users.noreply.github.com>
2022-02-08 23:16:11 +0300

    Add a test check login

--- src/test/java/tests/TestingTest.java
+++ src/test/java/tests/TestingTest.java
@@ -3,6 +3,7 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -21,7 +22,7 @@ public class TestingTest {
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, 4);
+        wait = new WebDriverWait(driver, 20);
     }
 
     @AfterMethod(alwaysRun = true)
@@ -33,9 +34,17 @@ public class TestingTest {
     public void testingTest() {
         driver.get("https://www.google.com/");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[name='q']")));
-
-
-
     }
 
+    @Test
+    public void checkLogin() {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.cssSelector("tr input[name='username']")).sendKeys("Choson");
+        driver.findElement(By.cssSelector("tr input[name='password']")).sendKeys("1234");
+        driver.findElement(By.cssSelector("tr input[type='checkbox']")).click();
+        WebElement button = driver.findElement(By.cssSelector("button[type='submit']"));
+        button.click();
+        wait.until(ExpectedConditions.invisibilityOf(button));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='notice errors']")));
+    }
 }
